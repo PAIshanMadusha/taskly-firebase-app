a43b865ab2465d3b05242451f48211e8bfb7d5bd
Ishan200207 <168297757+Ishan200207@users.noreply.github.com>
2025-03-03 14:23:45 +0530

    Firebase Initial Configurations

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Taskly Firebase App",
       home: Scaffold(),
     );
   }
